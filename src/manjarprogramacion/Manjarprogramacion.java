b7cf870f3e33980692f4c72a0bcffd6997a8da46
galle <galle@192.168.0.16>
2016-03-07 20:33:09 +0100

    - Añadido metodo para ordenar arraylist. - Añadido metodo para añadir puntuacion al arrayList. - Añadido metodo para volcar arryList en el archivo jugadores.txt.

--- src/manjarprogramacion/Manjarprogramacion.java
+++ src/manjarprogramacion/Manjarprogramacion.java
@@ -13,21 +13,21 @@ public class Manjarprogramacion {
         
         for (boolean b = false; b == false;) {
 
-            switch (Integer.parseInt(JOptionPane.showInputDialog("la opcion:\n\n1.) Empezar\n2.) \n3.) \n4.) \n5.) \n6.) \n7.) Salir"))) {
+            switch (Integer.parseInt(JOptionPane.showInputDialog("La opcion:\n\n1.) Reproduce un campeon aleatorio\n2.) Leer archivo de jugadores y cargar en el arrayList\n3.) Añadir una puntuacion\n4.) Ordenar Arrylist por orden de puntuaciones\n5.) Escribir puntuaciones en archivo\n6.) \n7.) Salir"))) {
                 case 1:
                     obj1.leerArchivo(obj1.generarNumero());
                     break;
                 case 2:
-                    
+                    obj1.leerArchivoPuntuaciones();
                     break;
                 case 3:
-                        
+                    obj1.añadirPuntuacion();
                     break;
                 case 4:
-                    
+                    obj1.ordenarArray();
                     break;
                 case 5:
-                    
+                    obj1.escribirArchivoPuntuaciones ();
                     break;
                 case 6:
                     
--- src/manjarprogramacion/Metodos.java
+++ src/manjarprogramacion/Metodos.java
@@ -3,6 +3,9 @@ package manjarprogramacion;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -15,15 +18,15 @@ import javax.sound.sampled.UnsupportedAudioFileException;
  */
 public class Metodos {
 
-    public Clip clip ;
+    public Clip clip;
     public String ruta = "/voces/";
+    Jugadores objJugadores;
+    ArrayList <Jugadores> arrayJugadores = new ArrayList();
 
     public int generarNumero() {
-        int numeroAleatorio = (int) (Math.random() * 129+1);
+        int numeroAleatorio = (int) (Math.random() * 129 + 1);
         return numeroAleatorio;
     }
-
-    //metodo sin hacer
 
     public void leerArchivo(int n) {
         File f;
@@ -31,24 +34,24 @@ public class Metodos {
         try {
             f = new File("src/manjarprogramacion/voces.txt");
             sc = new Scanner(f);
-            
+
             while (sc.hasNextLine()) {
                 String res = sc.nextLine();
                 System.out.println(res);
-                String [] arrayNumeroYNombre = res.split(",");   
+                String[] arrayNumeroYNombre = res.split(",");
                 if (Integer.parseInt(arrayNumeroYNombre[0]) == n) {
                     System.out.println(arrayNumeroYNombre[1]);
                     sonido(arrayNumeroYNombre[1]);
                 }
             }
         } catch (FileNotFoundException ex) {
-            System.out.println("Error 1 " + ex.toString());
+            System.out.println("Archivo no encontrado");
         } finally {
             sc.close();
         }
     }
 
-    public void sonido (String archivo) {
+    public void sonido(String archivo) {
 
         try {
             clip = AudioSystem.getClip();
@@ -57,6 +60,57 @@ public class Metodos {
         } catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
         }
     }
+
+    public void leerArchivoPuntuaciones() {
+        
+        File f;
+        Scanner sc = null;
+        try {
+            f = new File("src/manjarprogramacion/jugadores.txt");
+            sc = new Scanner(f);
+            while (sc.hasNextLine()) {
+                String res = sc.nextLine();
+                String[] arrayPuntuacionYNombre = res.split(",");
+                objJugadores= new Jugadores(Integer.parseInt(arrayPuntuacionYNombre[0]),arrayPuntuacionYNombre[1]);
+                arrayJugadores.add(objJugadores);
+                
+            }
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Archivo no encontrado");
+        } finally {
+            sc.close();
+        }
+    }
+    public void ordenarArray (){
+        for(Jugadores j:arrayJugadores){
+            System.out.println(j.toString());
+        }
+        Collections.sort(arrayJugadores);
+        for(Jugadores j:arrayJugadores){
+            System.out.println(j.toString());
+        }
+    }
+    public void escribirArchivoPuntuaciones (){
+        File f;
+        PrintWriter pr = null;
+        try {
+            f = new File ("src/manjarprogramacion/jugadores.txt");
+            pr = new PrintWriter (f);
+            for(Jugadores j:arrayJugadores){
+            pr.println (j.puntuacionNombre());
+            }
+        }
+        catch (FileNotFoundException ex){
+            System.out.println("Archivo no encontrado");
+        }
+        finally {
+            pr.close();
+        }
+    }
     
-    
+    public void añadirPuntuacion (){
+        objJugadores = new Jugadores (800,"Jorge");
+        arrayJugadores.add(objJugadores);
+    }
 }
