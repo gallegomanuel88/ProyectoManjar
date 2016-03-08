0ec263d0e33133dcf54cdb669881b9a1066be1f4
galle <galle@192.168.0.16>
2016-03-08 18:27:22 +0100

    - Corregido el metodo comprobarCampeon a la hora de sumar puntos.

--- src/manjarprogramacion/Metodos.java
+++ src/manjarprogramacion/Metodos.java
@@ -11,6 +11,7 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -20,14 +21,26 @@ public class Metodos {
 
     public Clip clip;
     public String ruta = "/voces/";
+    public String campeonActual;
+    public String jugadorActual;
+    public int puntosActual;
     Jugadores objJugadores;
     ArrayList <Jugadores> arrayJugadores = new ArrayList();
 
+    /**
+     * Genera un numero aleatorio entre el 1 y el 129.
+     * @return retorna el numero aleatorio generado.
+     */
     public int generarNumero() {
         int numeroAleatorio = (int) (Math.random() * 129 + 1);
         return numeroAleatorio;
     }
 
+    /**
+     * Este metodo lee el archivo "voces.txt" y busca el numero del campeon que proporciona la variable "int n". Cuando encuentra el numero del campeon llama al metodo sonido(nombre del campeon).
+     * 
+     * @param n este parametro va a ser comparado con el numero del campeon buscado en el archivo. Este numero sera el return del metodo generarNumero.
+     */
     public void leerArchivo(int n) {
         File f;
         Scanner sc = null;
@@ -37,11 +50,12 @@ public class Metodos {
 
             while (sc.hasNextLine()) {
                 String res = sc.nextLine();
-                System.out.println(res);
+                //System.out.println(res);
                 String[] arrayNumeroYNombre = res.split(",");
                 if (Integer.parseInt(arrayNumeroYNombre[0]) == n) {
                     System.out.println(arrayNumeroYNombre[1]);
                     sonido(arrayNumeroYNombre[1]);
+                    campeonActual = arrayNumeroYNombre[1];
                 }
             }
         } catch (FileNotFoundException ex) {
@@ -51,6 +65,10 @@ public class Metodos {
         }
     }
 
+    /**
+     * Con el String que recibe abre un archivo de sonido .wav con el mismo nombre que el contenido del String.
+     * @param archivo "String archivo" va a recibir un String que correspondera con el nombre de uno de los archivos "campeon.wav".
+     */
     public void sonido(String archivo) {
 
         try {
@@ -61,6 +79,9 @@ public class Metodos {
         }
     }
 
+    /**
+     * Este metodo lee el archivo "jugadores.txt" y carga los los datos leidos en el arrayList "arrayJugadores" de clase <jugadores>.
+     */
     public void leerArchivoPuntuaciones() {
         
         File f;
@@ -73,7 +94,7 @@ public class Metodos {
                 String[] arrayPuntuacionYNombre = res.split(",");
                 objJugadores= new Jugadores(Integer.parseInt(arrayPuntuacionYNombre[0]),arrayPuntuacionYNombre[1]);
                 arrayJugadores.add(objJugadores);
-                
+                campeonActual=arrayPuntuacionYNombre[1];
             }
 
         } catch (FileNotFoundException ex) {
@@ -82,15 +103,23 @@ public class Metodos {
             sc.close();
         }
     }
+    
+    /**
+     * Este metodo ordena el arrayList "arrayJugadores" de clase <jugadores> cargado anteriormente por el metodo leerPuntuaciones ().
+     */
     public void ordenarArray (){
-        for(Jugadores j:arrayJugadores){
-            System.out.println(j.toString());
-        }
+        //for(Jugadores j:arrayJugadores){
+        //    System.out.println(j.toString());
+        //}
         Collections.sort(arrayJugadores);
-        for(Jugadores j:arrayJugadores){
-            System.out.println(j.toString());
-        }
+        //for(Jugadores j:arrayJugadores){
+        //   System.out.println(j.toString());
+        //}
     }
+    
+    /**
+     * Este metodo sobreescribe los datos almacenados en el arrayList "arrayJugadores" de clase <jugadores>, en el archivo "jugadores.txt".
+     */
     public void escribirArchivoPuntuaciones (){
         File f;
         PrintWriter pr = null;
@@ -109,8 +138,59 @@ public class Metodos {
         }
     }
     
+    /**
+     * Este metodo añade una puntuacion y jugador al arraylist "arrayJugadores" de clase <Jugadores>.
+     */
     public void añadirPuntuacion (){
-        objJugadores = new Jugadores (800,"Jorge");
+        objJugadores = new Jugadores (puntosActual,JOptionPane.showInputDialog("Escribe tu nombre de invocador"));
         arrayJugadores.add(objJugadores);
+    }
+    
+    public void comprobarCampeon (){
+        String campeon = JOptionPane.showInputDialog("Introduce el nombre del campeon:");
+        if (campeon.equalsIgnoreCase(campeonActual)){
+            JOptionPane.showMessageDialog(null, "Has acertado");
+            puntosActual= (puntosActual+5);
+        } 
+    }
+    //NO USAR ESTES METODOS EN JFRAME
+    public void imprimir (){
+        for(Jugadores j:arrayJugadores){
+            System.out.println(j.toString());
+        }    
+    }
+    public void jugar(){
+        leerArchivoPuntuaciones();
+        for (boolean b = false; b == false;) {
+
+            switch (Integer.parseInt(JOptionPane.showInputDialog("La opcion:\n\n1.) Jugar\n2.) Ver puntuaciones\n3.) Salir"))) {
+                case 1:
+                    puntosActual=0;
+                    leerArchivo(generarNumero());
+                    comprobarCampeon ();
+                    leerArchivo(generarNumero());
+                    comprobarCampeon ();
+                    leerArchivo(generarNumero());
+                    comprobarCampeon ();
+                    leerArchivo(generarNumero());
+                    comprobarCampeon ();
+                    leerArchivo(generarNumero());
+                    comprobarCampeon ();
+                    añadirPuntuacion ();
+                    ordenarArray ();
+                    escribirArchivoPuntuaciones ();
+                    break;
+                case 2:
+                    ordenarArray();
+                    imprimir();
+                    break;
+                case 3:
+                    b = true;
+                    break;
+                default:
+                    break;
+            }
+
+        }
     }
 }
