62d6137b4fe254296d8893e8ec1d023fef2d02ad
oracle <oracle@atanasoff08.danielcastelao.org>
2016-03-10 12:36:50 +0100

    - Ya entendimos JFrame, 3ev. aprovada

--- src/JFrame/MenuPanel.java
+++ src/JFrame/MenuPanel.java
@@ -6,7 +6,7 @@
 package JFrame;
 
 import javax.swing.JOptionPane;
-
+import manjarprogramacion.*;
 /**
  *
  * @author oracle
@@ -19,7 +19,7 @@ public class MenuPanel extends javax.swing.JFrame {
     public MenuPanel() {
         initComponents();
     }
-
+    Metodos obx_metodos = new Metodos();
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -84,11 +84,11 @@ public class MenuPanel extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        setVisible(false);
+        obx_metodos.leerArchivo(obx_metodos.generarNumero());
         PanelCanciones jFrame = new PanelCanciones();
-        JOptionPane.showMessageDialog(null, "Presiona 'Aceptar' cuando estés listo");
         jFrame.setVisible(true);
-
-
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -98,37 +98,7 @@ public class MenuPanel extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MenuPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MenuPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MenuPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MenuPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MenuPanel().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
--- src/manjarprogramacion/Manjarprogramacion.java
+++ src/manjarprogramacion/Manjarprogramacion.java
@@ -1,6 +1,6 @@
 package manjarprogramacion;
 
-import javax.swing.JOptionPane;
+import JFrame.MenuPanel;
 
 /**
  *
@@ -9,43 +9,11 @@ import javax.swing.JOptionPane;
 public class Manjarprogramacion {
 
     public static void main(String[] args) {
-        Metodos obj1 = new Metodos ();
         
-        for (boolean b = false; b == false;) {
-
-            switch (Integer.parseInt(JOptionPane.showInputDialog("La opcion:\n\n1.) Reproduce un campeon aleatorio\n2.) Leer archivo de jugadores y cargar en el arrayList\n3.) Añadir una puntuacion\n4.) Ordenar Arrylist por orden de puntuaciones\n5.) Escribir puntuaciones en archivo\n6.) Jugar\n7.) Salir"))) {
-                case 1:
-                    obj1.leerArchivo(obj1.generarNumero());
-                    break;
-                case 2:
-                    obj1.leerArchivoPuntuaciones();
-                    break;
-                case 3:
-                    obj1.añadirPuntuacion();
-                    break;
-                case 4:
-                    obj1.ordenarArray();
-                    break;
-                case 5:
-                    obj1.escribirArchivoPuntuaciones ();
-                    break;
-                case 6:
-                    obj1.jugar();
-                    system("CLS");
-                    break;
-                case 7:
-                    b = true;
-                    break;
-
-                default:
-                    break;
-            }
-
-        }
+        MenuPanel mp = new MenuPanel();
+        mp.setVisible(true);
+        Metodos obx_metodos = new Metodos();
+        
+        
     }
-
-    private static void system(String cls) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
